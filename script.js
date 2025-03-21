ba3476de0e6c8dfb6c71fc85327a7ad1cd77f2a1
froge223 <2492201@gmail.com>
2025-03-21 12:49:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const message = document.getElementById('message');
 
 button.addEventListener('click', () => {
     button.style.display = 'none'; // Скрываем кнопку
-    message.classList.remove('hidden'); // Показываем сообщение и картинку
+    message.classList.remove('hidden'); // Показываем сообщение
+    message.classList.add('visible'); // Добавляем анимацию появления
 });
